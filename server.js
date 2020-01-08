18ff96b1378116da90338c322070597533e29e5a
Rob Spears <rob.sp3ars@gmail.com>
2020-01-08 11:38:21 -0500

    Method chain server's listen statement

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ import swaggerDocRouter from "./SwaggerDocRouter";
 const server = express();
 const port = process.env.PORT || 3000;
 
-server.use("/", swaggerDocRouter);
-
-const app = server.listen(port, () => {
+server.use("/", swaggerDocRouter).listen(port, () => {
   console.log(`Server is now listening on port ${port}`);
 });
