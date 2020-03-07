99cb23370f5eb0f137c2a766eab9944e5f89c594
Rob Spears <rob.sp3ars@gmail.com>
2020-03-06 19:39:43 -0500

    Add controller for user logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,12 +5,32 @@ import jwt from "jsonwebtoken";
 
 export const userLogin = (request, response) => {
   const { username, password } = request.body;
+
+  // username/email and password inclusion validation
+  if (!username) {
+    response.statusCode = 400;
+    response.send({
+      message: "Enter a username/email address",
+      statusCode: response.statusCode
+    });
+  }
+
+  if (!password) {
+    response.statusCode = 400;
+    response.send({
+      message: "Enter a password",
+      statusCode: response.statusCode
+    });
+  }
+
+  // query database to get user information
   User.findOne({
     where: {
       [Op.or]: [{ username }, { email: username }]
     }
   })
     .then(async user => {
+      // generate json web token if username/email and password exist
       if (user && (await bcrypt.compare(password, user.password))) {
         const token = jwt.sign(
           { username: user.username },
@@ -37,4 +57,21 @@ export const userLogin = (request, response) => {
         statusCode: response.statusCode
       });
     });
+};
+
+export const userLogout = (request, response) => {
+  const { token } = request.body;
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      response.statusCode = 401;
+      response.send({
+        message: "Unauthorized to make request",
+        statusCode: response.statusCode
+      });
+    } else {
+      response.send({
+        statusCode: 200
+      });
+    }
+  });
 };
